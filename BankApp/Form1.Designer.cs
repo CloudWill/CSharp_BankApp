36bac05a021750bf3a34463c2a198ea5f8192145
Willy S <willykwsin@gmail.com>
2024-05-24 00:27:27 -0700

    initial commit

---
+++ BankApp/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BankApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenAccount = new System.Windows.Forms.Button();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenAccount
+            // 
+            this.btnOpenAccount.Location = new System.Drawing.Point(12, 418);
+            this.btnOpenAccount.Name = "btnOpenAccount";
+            this.btnOpenAccount.Size = new System.Drawing.Size(105, 20);
+            this.btnOpenAccount.TabIndex = 0;
+            this.btnOpenAccount.Text = "Open an Account";
+            this.btnOpenAccount.UseVisualStyleBackColor = true;
+            this.btnOpenAccount.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(352, 201);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(35, 13);
+            this.lblTest.TabIndex = 1;
+            this.lblTest.Text = "label1";
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(432, 338);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(108, 43);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "GoTwo";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.btnOpenAccount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenAccount;
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
